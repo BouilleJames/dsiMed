acb3e48bd4f1ac7d7242ca6d3f9a834c9a93e691
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-03-07 17:49:43 +0100

    add-search

---
+++ app.js
@@ -0,0 +1,40 @@
+// fenetre pop-up
+
+// function ouvrirPopup() {
+//     window.open("https://www.example.com", "nom_de_la_fenetre", "height=400,width=400");
+//   }
+ 
+
+// carroussel
+
+// Récupération des éléments HTML
+// const carousel = document.querySelector('.carousel');
+// const slides = carousel.querySelectorAll('.slide');
+// const prevBtn = carousel.querySelector('.prev-btn');
+// const nextBtn = carousel.querySelector('.next-btn');
+
+// // Initialisation des variables
+// let currentSlide = 0;
+// const slideWidth = slides[0].clientWidth;
+
+// // Fonction pour afficher le slide suivant
+// function nextSlide() {
+//   currentSlide++;
+//   if (currentSlide >= slides.length) {
+//     currentSlide = 0;
+//   }
+//   carousel.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+// }
+
+// // Fonction pour afficher le slide précédent
+// function prevSlide() {
+//   currentSlide--;
+//   if (currentSlide < 0) {
+//     currentSlide = slides.length - 1;
+//   }
+//   carousel.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+// }
+
+// // Ajout des écouteurs d'événements sur les boutons
+// nextBtn.addEventListener('click', nextSlide);
+// prevBtn.addEventListener('click', prevSlide);
