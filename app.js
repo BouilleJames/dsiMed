c05e885159124597f42a20ea14cb220d618d2ac3
BouilleJames <77192627+BouilleJames@users.noreply.github.com>
2023-03-18 17:01:49 +0100

    add

--- app.js
+++ app.js
@@ -38,3 +38,46 @@
 // // Ajout des écouteurs d'événements sur les boutons
 // nextBtn.addEventListener('click', nextSlide);
 // prevBtn.addEventListener('click', prevSlide);
+
+
+document.body.onload=function(){
+    nbr=8;
+    p=0;
+    container=document.getElementById("container");
+    g=document.getElementById("g");
+    d=document.getElementById("d");
+    container.style.width=(800*nbr)+"px";
+    for(i=1;i<=nbr;i++){
+        div=document.createElement("div");
+        div.className="photo";
+        div.style.backgroundImage="url('image/carroussel"+i+".jpg')";
+        container.appendChild(div);
+    }
+    afficherMasquer();
+}
+g.onclick=function() {
+    if(p>-nbr+1)
+        p--;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+}
+d.onclick=function() {
+    if(p<0)
+        p++;
+    container.style.transform="translate("+p*800+"px)";
+    container.style.transition="all 0.5s ease";
+    afficherMasquer();
+}
+function afficherMasquer(){
+    if(p==-nbr+1)
+        g.style.visibility="hidden";
+    else
+        g.style.visibility="visible";
+     if(p==0)
+        d.style.visibility="hidden";
+     else
+        d.style.visibility="visible";
+}
+
+
